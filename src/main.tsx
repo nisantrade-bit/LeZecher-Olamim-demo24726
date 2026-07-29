7f3b01a132e99eae70bcce47d68a636a700f3639
nisantrade-bit <nisantrade@gmail.com>
2026-07-30 02:29:55 +0300

    Refactor ReactDOM import and rendering method

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './src/App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './src/App.tsx';
 import './index.css';
 
-const rootElement = document.getElementById('root');
-
-if (rootElement) {
-  createRoot(rootElement).render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
